1b98adf6747b6e531b3d20085787dcdf96a6234e
yes-2875 <mirzafarrukh97@gmail.com>
2025-11-18 22:51:24 -0500

    Completed Part 3 - Destructuring Assignment

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ console.log("student.name: " + student.name);
 console.log("student.age: " + student.age);
 console.log(student.displayInfo());
 
+
 // Part 2: Working with JSON
 // Convert student into a JSON string using JSON.stringify();
 const stringified = JSON.stringify(student);
@@ -37,3 +38,13 @@ const newStudent = JSON.parse(stringified);
 console.log("Both student objects (original and parsed):");
 console.log(student);
 console.log(newStudent);
+console.log("They're not the same anymore because student was created from the Student constructor.");
+
+
+// Part 3: Using Destructuring Assignment
+// Use destructuring to extract name and courses from student object.
+const {name, courses} = student;
+
+// Create an array of scores, then destructure the first two scores from it.
+const scores = [29, 63, 91, 95, 82, 67];
+const [first, second] = scores;

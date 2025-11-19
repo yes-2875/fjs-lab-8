43516d0cda48fab4ded998b8fbba15b32a0da75c
yes-2875 <mirzafarrukh97@gmail.com>
2025-11-18 22:37:11 -0500

    Created instance of Student object and output properties

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function Student(name, age, enrolled, courses) {
         return `Student Name: ${this.name}\nAge: ${this.age} years old\nIs enrolled: ${this.enrolled}\nCourses: ${this.courses}`;
     }
 }
+
+const student = new Student(
+    "Mirza",
+    18,
+    true,
+    ["CPAN 111", "CPAN 116", "CPAN 133", "CPAN 113", "CPAN 134", "WRIT 120"]
+);
+
+console.log("student.name: " + student.name);
+console.log("student.age: " + student.age);
+console.log(student.displayInfo());
